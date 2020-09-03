b9b97823784358dba48b3095be55df7e99e09ea6
aidan campbell <campbella@hartwick.edu>
2020-09-03 13:00:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Start typing your program below this line..
+print ("Hello World!")
+print ("What is your name?")
+myName = input ()
+print ('It is good to meet you, ' + myName)
+print ('How is your day going?')
+go = input ()
+print ('That is ' + go)
+print ('So, ' + myName  +  ', What is your favorite food?')
+fav = input ()
+print ('No way! My favorite food is '  +  fav  +  ' too!')
+color = input ("What is your favorite color?")
+if color.lower() == 'red':
+  print ('I like that color too!')
+else:
+  print ('I prefer red.')
+
